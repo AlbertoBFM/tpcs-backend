5073eb9a6806d7da77fabdbab0b4e5855fa82f8b
AlbertoBFM <alberto.brandon2@gmail.com>
2022-11-16 15:26:06 -0400

    Modificamos el package.json

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ dbConnection();
 app.use(cors());
 
 // Public directory
-app.use( express.static('public') );
+app.use( express.static( __dirname + '/public' ));
 
 // Read y Pars of body
 
 app.use( express.json() );
-
 
 // Routes
 app.use( '/api/auth', require('./routes/auth') );
@@ -30,6 +29,27 @@ app.use( '/api/sale', require('./routes/sale') );
 app.use( '/api/saleDetail', require('./routes/saleDetail') );
 app.use( '/api/report', require('./routes/report') );
 app.use( '/api/user', require('./routes/user') );
+
+app.get( '*', ( req, res ) => {
+    res.sendFile( __dirname + '/public' );
+});
+// // Public directory
+// app.use( express.static('public') );
+
+// // Read y Pars of body
+
+// app.use( express.json() );
+
+
+// // Routes
+// app.use( '/api/auth', require('./routes/auth') );
+// app.use( '/api/category', require('./routes/category') );
+// app.use( '/api/provider', require('./routes/provider') );
+// app.use( '/api/product', require('./routes/product') );
+// app.use( '/api/sale', require('./routes/sale') );
+// app.use( '/api/saleDetail', require('./routes/saleDetail') );
+// app.use( '/api/report', require('./routes/report') );
+// app.use( '/api/user', require('./routes/user') );
 
 // Listen requests
 app.listen( process.env.PORT, () => {
