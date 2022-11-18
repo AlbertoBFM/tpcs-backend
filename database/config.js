518a9ea2af6322ffacf5d50afc7ffe7cda696ed0
AlbertoBFM <alberto.brandon2@gmail.com>
2022-11-18 15:25:57 -0400

    Cambio de Cluster

--- database/config.js
+++ database/config.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-
+        console.log('Connecting...');
         await mongoose.connect( process.env.DB_CNN );
         // await mongoose.connect(process.env.MONGODB_URI);
-    
         console.log('DB Online');
-
     } catch ( error ) {
         console.log( error );
         throw new mongoose.Error('Error al conectar con la BD');
